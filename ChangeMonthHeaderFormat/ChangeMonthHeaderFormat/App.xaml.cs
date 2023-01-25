d8b0945ab4651da7a8d69aeb97bf7f43edb22be8
VishalOmprasad <vishal.omprasad@syncfusion.com>
2023-01-25 11:36:41 +0530

    New KB sample added

---
+++ ChangeMonthHeaderFormat/ChangeMonthHeaderFormat/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ChangeMonthHeaderFormat;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ ChangeMonthHeaderFormat/ChangeMonthHeaderFormat/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ChangeMonthHeaderFormat;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
